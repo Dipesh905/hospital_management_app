d85213fa891bc6bc965f22f74cc9e840ead29ee2
Dipesh Ghi <ghimiredipesh5@gmail.com>
2023-10-13 14:26:23 +0545

    folder structure and lab order screen added

---
+++ lib/doctor_module/model/lab_requisition_model.dart
@@ -0,0 +1,15 @@
+class LabRequisitionModel {
+  String patientName;
+  String patientAddress;
+  int patientAge;
+  String labTestName;
+  String labOrderDate;
+
+  LabRequisitionModel({
+    required this.patientName,
+    required this.patientAddress,
+    required this.patientAge,
+    required this.labTestName,
+    required this.labOrderDate,
+  });
+}
---
+++ lib/doctor_module/view/lab_order_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hospital_management_app/common_module/button_widget.dart';
+import 'package:hospital_management_app/common_module/input_field_widget.dart';
+
+class LabOrderScreen extends StatelessWidget {
+  LabOrderScreen({super.key});
+
+  final TextEditingController _personNameController = TextEditingController();
+  final TextEditingController _personAddressController =
+      TextEditingController();
+  final TextEditingController _personAgeController = TextEditingController();
+  final TextEditingController _labTestNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Lab Requisition Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            InputFieldWidget(
+              controller: _personNameController,
+              labelText: 'Person Name',
+            ),
+            InputFieldWidget(
+              controller: _personAddressController,
+              labelText: 'Person Address',
+            ),
+            InputFieldWidget(
+              controller: _personAgeController,
+              labelText: 'Person Age',
+            ),
+            InputFieldWidget(
+              controller: _labTestNameController,
+              labelText: 'Lab Test Name',
+            ),
+            ButtonWidget(
+              buttonTitle: 'Create Lab Order',
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
